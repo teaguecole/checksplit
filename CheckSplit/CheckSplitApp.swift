d4b019fc6f1da36dd68049dc27da50c56863424b
Teague Cole <tnc0419@icloud.com>
2022-02-14 22:32:50 -0500

    Initial Commit

---
+++ CheckSplit/CheckSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplitApp.swift
+//  CheckSplit
+//
+//  Created by Teague Cole on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
